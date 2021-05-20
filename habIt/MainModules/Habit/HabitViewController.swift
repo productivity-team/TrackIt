5f32540723b85e6fb7234bc54d332d7d5c73ab2f
Maria <mapecheritsyna@gmail.com>
2021-05-20 15:10:07 +0300

    Полностью переделала фронт экрана привычки и сделала круговой индикатор прогресса

--- habIt/MainModules/Habit/HabitViewController.swift
+++ habIt/MainModules/Habit/HabitViewController.swift
@@ -1,34 +1,34 @@
+
 //
 //  HabitViewController.swift
 //  habIt
 //
 //  Created by Maria Pecheritsyna on 22.04.2021.
-//  
+//
 //
 
 import UIKit
 
 final class HabitViewController: UIViewController {
     private let output: HabitViewOutput
-    private let habitnameLable = UILabel()
-    private let progressLable = UILabel()
-    private let journalLable = UILabel()
-    private let unitsLable = UILabel()
-    private let addLable = UILabel()
     private let unitsboxImage = UIImageView(image: UIImage(named: "textbox"))
     private let topbarImage = UIImageView(image: UIImage(named: "topbar"))
-    private let iconImage = UIImageView()
-    private let settingsiconButton = UIButton()
-    private let subtrButton = UIButton()
-    private let pr10Button = UIButton()
-    private let pr25Button = UIButton()
-    private let pr50Button = UIButton()
-    private let pr75Button = UIButton()
-    private let addButton = UIButton()
-    private let progressbarPView = UIProgressView()
-    private let unitsField = UITextField()
-    private let journalField = UITextView()
-    private var placeholder = "Запиши сюда свои наблюдения"
+
+    private let habitName = UILabel() //название привычки
+    private let completionsLabel = UILabel() //сколько выполнено
+    private let dividerLabel = UILabel() // "из"
+    private let targetLabel = UILabel() //цель привычки
+    private let unitsLabel = UILabel() //единицы измерения
+    private let settingsiconButton = UIButton() //редактировать привычку
+    private let unitsField = UITextField() //textfield для добавления прогресса
+    private let addButton = UIButton() //кнопка добавления прогресса
+    private let resetButton = UIButton() //кнопка сброса прогресса
+    private let circularProgress = CircularProgress(frame: CGRect(x: 10.0, y: 30.0, width: 305.0, height: 305.0))
+    private let screenColor = UIColor(red: 0/255, green: 230/255, blue: 190/255, alpha: 1)
+    
+    private var numberOfCompletions = 0
+    private let target = 10
+
     
     init(output: HabitViewOutput) {
         self.output = output
@@ -46,65 +46,54 @@ final class HabitViewController: UIViewController {
         
         view.backgroundColor = UIColor(named: "Background1")
         
+        habitName.text = "Выпить воды"
+        habitName.font = UIFont(name: "Lato-Regular", size: 25)
+        
+        
+        circularProgress.progressColor = screenColor
+        circularProgress.trackColor = UIColor.lightGray
+        circularProgress.tag = 101
+        circularProgress.center = self.view.center
+//        self.view.addSubview(circularProgress)
+        
+        //animate progress
+        self.perform(#selector(animateProgress), with: nil, afterDelay: 0.3)
+        
+        
+        completionsLabel.text = numberOfCompletions.description
+        completionsLabel.textColor = screenColor
+        completionsLabel.font = UIFont(name: "Lato-Bold", size: 35)
+        
+        dividerLabel.text = "из"
+        dividerLabel.textColor = screenColor
+        dividerLabel.font = UIFont(name: "Lato-Bold", size: 35)
+        
+        targetLabel.text = target.description
+        targetLabel.textColor = screenColor
+        targetLabel.font = UIFont(name: "Lato-Bold", size: 35)
+        
+        addButton.setTitle("Добавить", for: .normal)
+        addButton.titleLabel?.font = UIFont(name: "Lato-Medium", size: 20)
+        addButton.backgroundColor = UIColor(red: 182/255, green: 230/255, blue: 255/255, alpha: 1)
+        addButton.layer.cornerRadius = 10
+        addButton.setTitleColor(UIColor(red: 48/255, green: 48/255, blue: 48/255, alpha: 1), for: .normal)
+        
+        resetButton.setTitle("Сбросить прогресс", for: .normal)
+        resetButton.titleLabel?.font = UIFont(name: "Lato-Medium", size: 15)
+        resetButton.setTitleColor(UIColor(red: 123/255, green: 124/255, blue: 124/255, alpha: 1), for: .normal)
+        
         settingsiconButton.setImage(UIImage(named: "pencil"), for: .normal)
-        settingsiconButton.tintColor = UIColor(named: "Green")
-        
-        journalField.layer.cornerRadius = 7
-        journalField.backgroundColor = UIColor(named: "Textbox")
-        journalField.font = UIFont(name: "Lato-Regular", size: 18)
-        journalField.textColor = UIColor(named: "Text")
-        
-        journalLable.text = "Дневник"
-        journalLable.font = UIFont(name: "Lato-Medium", size: 20)
-        journalLable.textColor = UIColor(named: "Text")
-        
-        [pr10Button, pr25Button, pr50Button, pr75Button].forEach{
-            $0.backgroundColor = UIColor(named: "Green")    //Подкачивается из базы, лучше использовать переменную
-            $0.layer.cornerRadius = 7
-            $0.setTitleColor(UIColor(named: "Text"), for: .normal)
-            $0.titleLabel?.font = UIFont(name: "Lato-Medium", size: 18)
-        }
-        
-        pr10Button.setTitle("10%", for: .normal)    //Вычисляется на основе данных с базы
-        
-        pr25Button.setTitle("25%", for: .normal)    //Вычисляется на основе данных с базы
-        
-        pr50Button.setTitle("50%", for: .normal)    //Вычисляется на основе данных с базы
-        
-        pr75Button.setTitle("75%", for: .normal)    //Вычисляется на основе данных с базы
-        
-        subtrButton.setBackgroundImage(UIImage(named: "subtraction"), for: .normal)
-        subtrButton.alpha = 0.2     //Меняется в зависимости от активной кнопки
-        
-        addButton.setBackgroundImage(UIImage(named: "addition"), for: .normal)
-        addButton.alpha = 1     //Меняется в зависимости от активной кнопки
+        settingsiconButton.tintColor = screenColor
         
         unitsField.placeholder = "0"
         unitsField.textColor = UIColor(named: "Text")
         unitsField.font = UIFont(name: "Lato-Regular", size: 18)
+        unitsField.keyboardType = .numberPad
         
-        addLable.text = "Добавить"
-        addLable.font = UIFont(name: "Lato-Medium", size: 20)
-        addLable.textColor = UIColor(named: "Text")
+        unitsLabel.text = "Миллилитры"
+        unitsLabel.textColor = UIColor(named: "Green")
+        unitsLabel.font = UIFont(name: "Lato-Medium", size: 22)
         
-        unitsLable.text = "Миллилитры"
-        unitsLable.textColor = UIColor(named: "Green")
-        unitsLable.font = UIFont(name: "Lato-Medium", size: 14)
-        
-        progressbarPView.progress = 400/1000    //Потом лучше использовать две переменные
-        progressbarPView.progressTintColor = UIColor(named: "Green")
-        progressbarPView.backgroundColor = UIColor(named: "Textbox")
-        
-        progressLable.text = "400/1000"     //Подкачивается из базы
-        progressLable.textColor = UIColor(named: "Green")   //Подкачивается из базы, лучше использовать переменную
-        progressLable.font = UIFont(name: "Lato-Bold", size: 20)
-        
-        iconImage.tintColor = UIColor(named: "Green")   //Подкачивается из базы, лучше использовать переменную
-        iconImage.image = UIImage(named: "water")   //Подкачивается из базы
-        
-        habitnameLable.text = "Имя"    //Должно подкачиваться из базы
-        habitnameLable.font = UIFont(name: "Lato-Medium", size: 18)
-        habitnameLable.textColor = UIColor(named: "Text")
         
         
         let backBarButtonItem = UIBarButtonItem(title: "Назад", style: .plain, target: self, action: #selector(toMenuButtonPressed))
@@ -116,39 +105,8 @@ final class HabitViewController: UIViewController {
         self.navigationItem.rightBarButtonItem  = saveBarButtonItem
         
         
-        [topbarImage, habitnameLable, iconImage, progressLable, progressbarPView, unitsLable, addLable, unitsboxImage, unitsField, addButton, subtrButton, pr10Button, pr25Button, pr50Button, pr75Button, journalLable, journalField, settingsiconButton].forEach{view.addSubview($0)}
+        [topbarImage, habitName, circularProgress, completionsLabel, dividerLabel, targetLabel, unitsLabel, unitsboxImage, unitsField, addButton, resetButton, settingsiconButton].forEach{view.addSubview($0)}
     }
-    
-    
-
-    
-//    func textViewDidBeginEditing(_ textView: UITextView) {
-//        if journalField.textColor == .lightGray {
-//            journalField.text = ""
-//            journalField.textColor = .black
-//        }
-//    }
-//
-//    func textViewDidEndEditing(_ textView: UITextView) {
-//        if journalField.text.isEmpty {
-//            journalField.text = "Запиши сюда свои наблюдения"
-//            journalField.textColor = UIColor.lightGray
-//            placeholder = ""
-//        }
-//        else{
-//            placeholder = journalField.text
-//        }
-//    }
-//
-//    func textViewDidChange(_ textView: UITextView) {
-//        placeholder = journalField.text
-//    }
-//
-//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-//        return ((journalField.text.count) > 180 && journalField.frame.size.height > 100.0) ? false : true
-//    }
-    
-    
     
     
     override func viewDidLayoutSubviews() {
@@ -160,94 +118,59 @@ final class HabitViewController: UIViewController {
             .height(92)
             .width(440)
         
-        habitnameLable.pin
-            .top(52)
+        circularProgress.pin
+            .top(277)
+            .left(55)
             .sizeToFit()
+        
+        habitName.pin
+            .top(190)
             .hCenter()
-        
-        iconImage.pin
-            .below(of: topbarImage).marginVertical(40)
-            .left(35)
-            .height(40)
-            .width(40)
-        
-        progressLable.pin
-            .below(of: topbarImage).marginVertical(56)
-            .right(35)
             .sizeToFit()
         
-        progressbarPView.pin
-            .below(of: iconImage).marginVertical(10)
-            .height(5)
-            .width(344)
+        dividerLabel.pin
+            .top(402)
             .hCenter()
-        
-        unitsLable.pin
-            .below(of: progressbarPView).marginVertical(2)
-            .right(35)
             .sizeToFit()
         
-        addLable.pin
-            .left(35)
-            .below(of: progressbarPView).marginVertical(30)
+        completionsLabel.pin
+            .top(402)
+            .before(of: dividerLabel).marginHorizontal(6)
+            .sizeToFit()
+        
+        targetLabel.pin
+            .top(402)
+            .after(of: dividerLabel).marginHorizontal(6)
+            .sizeToFit()
+
+        unitsLabel.pin
+            .top(450)
+            .hCenter()
             .sizeToFit()
         
         unitsboxImage.pin
-            .below(of: addLable).marginVertical(25)
-            .after(of: addLable).marginHorizontal(-45)
+            .top(658)
+            .left(82)
             .height(50)
             .width(105)
         
         unitsField.pin
+            .top(658)
+            .left(96)
             .height(50)
             .width(75)
-            .below(of: addLable).marginVertical(25)
-            .after(of: addLable).marginHorizontal(-30)
         
         addButton.pin
-            .height(35)
-            .width(35)
-            .after(of: unitsboxImage).marginHorizontal(36)
-            .above(of: unitsboxImage).marginVertical(-43)
+            .height(50)
+            .width(118)
+            .top(658)
+            .right(82)
         
-        subtrButton.pin
-            .height(35)
-            .width(35)
-            .after(of: addButton).marginHorizontal(20)
-            .above(of: unitsboxImage).marginVertical(-43)
-        
-        [pr10Button, pr25Button, pr75Button, pr50Button].forEach{
-            $0.pin
-                .height(50)
-                .width(120)
-        }
-        
-        pr10Button.pin
-            .below(of: unitsboxImage).marginVertical(45)
-            .left(67)
-        
-        pr25Button.pin
-            .below(of: unitsboxImage).marginVertical(45)
-            .right(67)
-        
-        pr50Button.pin
-            .below(of: pr10Button).marginVertical(30)
-            .left(67)
-        
-        pr75Button.pin
-            .below(of: pr25Button).marginVertical(30)
-            .right(67)
-        
-        journalLable.pin
-            .sizeToFit()
-            .below(of: pr50Button).marginVertical(30)
-            .left(35)
-        
-        journalField.pin
-            .below(of: journalLable).marginVertical(25)
-            .left(35)
-            .height(100)
-            .width(344)
+        resetButton.pin
+            .top(732)
+            .height(18)
+            .width(145)
+            .hCenter()
         
         settingsiconButton.pin
             .right(40)
@@ -256,9 +179,17 @@ final class HabitViewController: UIViewController {
             .width(35)
     }
     
+    
+    @objc func animateProgress() {
+        let cp = self.view.viewWithTag(101) as! CircularProgress
+        cp.setProgressWithAnimation(duration: 1.0, value: 0.8)
+    }
+    
+    
     @objc func toMenuButtonPressed(){
         output.toMenuButtonPressed()
     }
+    
     
     @objc func saveHabitButtonPressed(){
         output.saveHabitButtonPressed()
